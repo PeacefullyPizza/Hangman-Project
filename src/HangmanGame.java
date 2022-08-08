a36c6b40e55ace2660d72f6f99412d74182eab9e
PeacefullyPizza <dylantimowens@gmail.com>
2022-08-08 11:13:11 -0230

    First commit for the HangmanGame. Just adding the psvm and setting up a Scanner.

---
+++ src/HangmanGame.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class HangmanGame {
+    private static int count = 0;
+
+
+    public static void main(String[] args) {
+        Scanner userGuess = new Scanner(System.in);
+
+        while (count < 7)
+            System.out.println("Guess a letter in the word!");
+
+
+    }
+}
