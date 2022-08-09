bd60120429c8b034784e7f96d3bc9613b8bba7f9
PeacefullyPizza <dylantimowens@gmail.com>
2022-08-09 16:53:12 -0230

    Cleaned up some code

--- src/HangmanGame.java
+++ src/HangmanGame.java
@@ -1,46 +1,50 @@
+import java.io.FileInputStream;
 import java.util.Random;
 import java.util.Scanner;
-import java.io.File;
 import java.io.FileNotFoundException;
 
 
 public class HangmanGame {
-    private static final int count = 0;
-
     public static final Random RANDOM = new Random();
-
     // The total amount of tries a user can have before "GAME OVER"
     public static final int failedAttempts = 7;
+    private static final int count = 0;
 
     public static void main(String[] args) {
         Scanner userGuess = new Scanner(System.in);
         try {
-            File level1Words = new File("Level1Words.txt");
-            File level2Words = new File("Level2Words.txt");
-            File level3Words = new File("Level3Words.txt");
-            Scanner level1Reader = new Scanner(level1Words);
-            Scanner level2Reader = new Scanner(level2Words);
-            Scanner level3Reader = new Scanner(level3Words);
-            while (level1Reader.hasNextLine()) {
+
+            Scanner level1Reader = new Scanner(new FileInputStream("Level1Words.txt"));
+            Scanner level2Reader = new Scanner(new FileInputStream("Level2Words.txt"));
+            Scanner level3Reader = new Scanner(new FileInputStream("Level3Words.txt"));
+
+            // Reading words from .txt files
+            while (level1Reader.hasNextLine() && level2Reader.hasNextLine() && level3Reader.hasNextLine()) {
                 String level1Data = level1Reader.nextLine();
                 System.out.println(level1Data);
-            }
-            level1Reader.close();
-            while (level2Reader.hasNextLine()) {
                 String level2Data = level2Reader.nextLine();
                 System.out.println(level2Data);
-            }
-            level2Reader.close();
-            while (level3Reader.hasNextLine()) {
                 String level3Data = level3Reader.nextLine();
                 System.out.println(level3Data);
+
             }
+//            level1Reader.close();
+//            while (level2Reader.hasNextLine()) {
+//                String level2Data = level2Reader.nextLine();
+//                System.out.println(level2Data);
+//            }
+//            level2Reader.close();
+//            while (level3Reader.hasNextLine()) {
+//                String level3Data = level3Reader.nextLine();
+//                System.out.println(level3Data);
+//            }
+            level1Reader.close();
+            level2Reader.close();
             level3Reader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-
     }
 
     // method for displaying ASCII ART
