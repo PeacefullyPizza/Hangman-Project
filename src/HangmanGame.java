1085b7b1147318b3a10913017682bbe3540d4cc4
PeacefullyPizza <dylantimowens@gmail.com>
2022-08-09 15:56:36 -0230

    Added txt files for 3 Levels. Also tested reading from the 3 files.

--- src/HangmanGame.java
+++ src/HangmanGame.java
@@ -1,14 +1,47 @@
+import java.util.Random;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 
 public class HangmanGame {
-    private static int count = 0;
+    private static final int count = 0;
 
+    public static final Random RANDOM = new Random();
+
+    // The total amount of tries a user can have before "GAME OVER" varies on Level selected
+    public static final int failedAttemptsLevel1 = 10;
+    public static final int failedAttemptsLevel2= 7;
+    public static final int failedAttemptsLevel3 = 5;
 
     public static void main(String[] args) {
         Scanner userGuess = new Scanner(System.in);
-
-        while (count < 7)
-            System.out.println("Guess a letter in the word!");
+        try {
+            File level1Words = new File("Level1Words.txt");
+            File level2Words = new File("Level2Words.txt");
+            File level3Words = new File("Level3Words.txt");
+            Scanner level1Reader = new Scanner(level1Words);
+            Scanner level2Reader = new Scanner(level2Words);
+            Scanner level3Reader = new Scanner(level3Words);
+            while (level1Reader.hasNextLine()) {
+                String level1Data = level1Reader.nextLine();
+                System.out.println(level1Data);
+            }
+            level1Reader.close();
+            while (level2Reader.hasNextLine()) {
+                String level2Data = level2Reader.nextLine();
+                System.out.println(level2Data);
+            }
+            level2Reader.close();
+            while (level3Reader.hasNextLine()) {
+                String level3Data = level3Reader.nextLine();
+                System.out.println(level3Data);
+            }
+            level3Reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
 
         //Nicks test commit
         System.out.println("Hello");
